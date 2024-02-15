af5811382f5d2a0d6b4c8cbcf9405c0196cbf46e
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-15 19:03:29 +0530

    model: display the face boundraies

--- model/mtcnn.py
+++ model/mtcnn.py
@@ -2,23 +2,20 @@ import cv2
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 
-# Load pre-trained models
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 mtcnn = MTCNN(keep_all=True, device=device)
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
-# Load image
 image_path = 'tests/1.jpg'
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Detect faces
 boxes, _ = mtcnn.detect(image)
 
-# Identify faces
 if boxes is not None:
     for box in boxes:
         box = box.astype(int)
+        cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
         face = image[box[1]:box[3], box[0]:box[2]]
         face = cv2.resize(face, (160, 160))
         face_tensor = torch.tensor(face).permute(2, 0, 1).unsqueeze(0).float().to(device)
@@ -28,3 +25,7 @@ if boxes is not None:
         print("Face Embedding:", embedding)
 else:
     print("No faces detected in the image.")
+
+cv2.imshow('Faces', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
