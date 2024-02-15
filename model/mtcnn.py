61b38fc70a5451d1bad8de45f228479672bdbe90
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-15 22:18:05 +0530

    model: added gaussian blur to detected face

--- model/mtcnn.py
+++ model/mtcnn.py
@@ -6,7 +6,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 mtcnn = MTCNN(keep_all=True, device=device)
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
-image_path = 'tests/1.jpg'
+image_path = 'tests/g1.png'
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -17,12 +17,14 @@ if boxes is not None:
         box = box.astype(int)
         cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
         face = image[box[1]:box[3], box[0]:box[2]]
+        blurred_face = cv2.GaussianBlur(face, (99, 99), 5)
+        image[box[1]:box[3], box[0]:box[2]] = blurred_face
         face = cv2.resize(face, (160, 160))
         face_tensor = torch.tensor(face).permute(2, 0, 1).unsqueeze(0).float().to(device)
         
-        embedding = resnet(face_tensor).detach().cpu().numpy().flatten()
+        # embedding = resnet(face_tensor).detach().cpu().numpy().flatten()
         
-        print("Face Embedding:", embedding)
+        # print("Face Embedding:", embedding)
 else:
     print("No faces detected in the image.")
 
