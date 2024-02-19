46fc12b3eb0bf3d59e175c4bc0cae9b5c070dac2
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-19 20:47:43 +0530

    model: completed flask api for face image blur

--- model/app.py
+++ model/app.py
@@ -1,7 +1,10 @@
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file, make_response, jsonify
+from mtcnn import FaceModel
+import cv2
+from io import BytesIO
 
 app = Flask(__name__)
+face = FaceModel()
 
 @app.route('/')
 def index():
@@ -17,9 +20,21 @@ def upload_file():
     if file.filename == '':
         return 'No selected file'
     
-    if file:
-        file.save(file.filename)
-        return 'File uploaded successfully'
+    try:
+        if file:
+            # file.save(file.filename)
+            image_stream = BytesIO()
+            image = face.detect(file.read())
+            success, encoded_image = cv2.imencode('.jpg', image)
+            if success:
+                image_stream.write(encoded_image.tobytes())
+                image_stream.seek(0)
+                response = make_response(send_file(image_stream, mimetype='image/jpeg'))
+                response.headers['Content-Disposition'] = f'attachment; filename={file.filename}'
+                return response
+
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
