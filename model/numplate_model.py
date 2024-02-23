c46290172203e8996f6634bb5fd4276ff60d1bf8
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-23 13:36:42 +0530

    model: Renamed the model file and added bounds for gaussian bluru

--- model/numplate_model.py
+++ model/numplate_model.py
@@ -9,7 +9,6 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 class NumplateModel:
     def __init__(self, trained_weights_path):
         self.model = tf.keras.models.load_model(trained_weights_path)
-    # model = tf.keras.models.load_model('')
 
     def detect(self, image):
         image1 = cv2.resize(image, (224, 224))
@@ -34,22 +33,22 @@ class NumplateModel:
         tl, br = coords
         center_x = (tl[0] + br[0]) // 2
         center_y = (tl[1] + br[1]) // 2
-
         nw = int((br[0] - tl[0]) * 2)  # new_width
         nh = int((br[1] - tl[1]) * 2)  # new_height
-
         new_tl = (center_x - nw // 2, center_y - nh // 2)
         new_br = (center_x + nw // 2, center_y + nh // 2)
-
+        new_tl = (max(new_tl[0], 0), max(new_tl[1], 0))
+        new_br = (min(new_br[0], image.shape[1]), min(new_br[1], image.shape[0]))
         mask = image.copy()
         cv2.rectangle(mask, new_tl, new_br, (255, 255, 255), -1)
         blurred_region = cv2.GaussianBlur(image[new_tl[1]:new_br[1], new_tl[0]:new_br[0]], (25, 25), 10)
         result = cv2.bitwise_and(image, mask)
         result[new_tl[1]:new_br[1], new_tl[0]:new_br[0]] = blurred_region
-        return result
+
+        return result   
 
 if __name__ == "__main__":
-    image_path = 'tests/c1.jpeg'
+    image_path = '/home/anurag/try/Automatic-License-Plate-Detection/data_images/train/N6.jpeg'
     file = open(image_path, 'rb')
     nparr = np.frombuffer(file.read(), np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
